c0d416659378b94890c999468ed809e93bc2a89d
jgeun <pjk8926@gmail.com>
2020-11-28 19:49:01 +0900

    Fourth Commit

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
+/*This is OssHw2_3 by park jung geun*/
 class Hello{
     public static void main(String[] args){
         System.out.println("this is Oss Hw2");
+        System.out.println("pull request study");
     }
 }
 
